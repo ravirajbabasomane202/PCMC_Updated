112f045ed8d4b72bafb5c21db36cfd4dd08952a0
ravirajbabasomane@gmail.com <demonlod202@gmail.com>
2026-04-17 12:56:43 +0530

    new Update

--- main_ui/lib/utils/constants.dart
+++ main_ui/lib/utils/constants.dart
@@ -3,19 +3,13 @@ import 'package:flutter/foundation.dart'
 
 class Constants {
   static String get baseUrl {
-    // Use API_BASE_URL from dart-define if available, otherwise use default
-    const apiBaseUrl = String.fromEnvironment('API_BASE_URL', defaultValue: 'https://pcmc-updated.onrender.com');
+    // Use API_BASE_URL from dart-define if available, otherwise use default.
+    // For mobile builds, this ensures the app points to the Render backend.
+    const apiBaseUrl = String.fromEnvironment(
+      'API_BASE_URL',
+      defaultValue: 'https://pcmcapp.onrender.com',
+    );
 
-    if (kIsWeb) return apiBaseUrl;
-
-    switch (defaultTargetPlatform) {
-      case TargetPlatform.android:
-      case TargetPlatform.iOS:
-      case TargetPlatform.macOS:
-      case TargetPlatform.windows:
-      case TargetPlatform.linux:
-      default:
-        return apiBaseUrl;
-    }
+    return apiBaseUrl;
   }
 }
